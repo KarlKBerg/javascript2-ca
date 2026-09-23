b2a254f0cf34892fb7b17588969ec517faae8052
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-23 18:22:05 +0200

    Created displayToastMessage function that renders toast message based on error or success

--- src/script.js
+++ src/script.js
@@ -37,3 +37,25 @@ async function registerUser() {
     displayToastMessage(`Error: ` + error, "error");
   }
 }
+
+function displayToastMessage(message, type) {
+  const CONTAINER = document.querySelector(".toast-message");
+  const MESSAGE_ELEMENT = document.createElement("span");
+  MESSAGE_ELEMENT.textContent = message;
+  MESSAGE_ELEMENT.classList.add("message-text");
+  if (!CONTAINER) return;
+  CONTAINER.appendChild(MESSAGE_ELEMENT);
+  if (type === "error") {
+    CONTAINER.classList.remove("hidden");
+    CONTAINER.classList.add("error");
+    setTimeout(() => {
+      CONTAINER.classList.add("hidden");
+    }, 3000);
+  } else {
+    CONTAINER.classList.remove("hidden");
+    CONTAINER.classList.add("success");
+    setTimeout(() => {
+      CONTAINER.classList.add("hidden");
+    }, 3000);
+  }
+}
