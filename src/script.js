2896876923d9296f02b9d376ea41fa52ed4a55dd
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-24 17:23:12 +0200

    Imported and added loginFormListener to listen for submit action on login form

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
 "use strict";
+import { loginFormListener } from "./components/LoginForm.js";
 import { registerFormListener } from "./components/RegisterForm.js";
 registerFormListener();
+loginFormListener();
