d83ee8b8a4e41bba871d4fcea06578457c027c47
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-25 11:19:06 +0200

    Added safeguard for when logout button is not in DOM

--- src/components/LogoutForm.js
+++ src/components/LogoutForm.js
@@ -2,6 +2,7 @@ import { logoutUser } from "../services/authService.js";
 
 export function logoutUserListener() {
   const logoutBtn = document.getElementById("logout-btn");
+  if (!logoutBtn) return;
   logoutBtn.addEventListener("click", (event) => {
     event.preventDefault();
     logoutUser();
