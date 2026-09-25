7662532522ca4b14f1aec252de8ddf2909eb3374
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-25 17:47:43 +0200

    Moved editPost and deletePost functions to PostCard

--- src/services/postsService.js
+++ src/services/postsService.js
@@ -5,24 +5,3 @@ export async function getPosts() {
   const response = await get(endpoint);
   return response.data;
 }
-
-function editPost() {}
-function deletePost() {}
-
-/*
-export async function getPosts() {
-  const endpoint = "social/posts?_author=true";
-  try {
-    const response = await get(endpoint);
-    const posts = response.data;
-    const container = document.querySelector(".posts-container");
-    if (!container) return;
-    container.innerHTML = "";
-    posts.forEach((feedPost) => {
-      renderPost(feedPost);
-    });
-  } catch (error) {
-    throw error;
-  }
-}
-*/
