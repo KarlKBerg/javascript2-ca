a31d50d3389aacc6056cd51793bbe50570a40d20
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-25 11:17:27 +0200

    Imported logoutUserListener and run it for the page to listen to the logout click

--- src/script.js
+++ src/script.js
@@ -1,5 +1,8 @@
 "use strict";
 import { loginFormListener } from "./components/LoginForm.js";
+import { logoutUserListener } from "./components/LogoutForm.js";
 import { registerFormListener } from "./components/RegisterForm.js";
+
 registerFormListener();
 loginFormListener();
+logoutUserListener();
