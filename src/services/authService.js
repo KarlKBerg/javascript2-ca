538ace0743088905ddebd174c046e45a7a2dc42e
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-25 19:33:18 +0200

    Removed trailing / on index

--- src/services/authService.js
+++ src/services/authService.js
@@ -46,5 +46,5 @@ export async function loginUser(credentials) {
 export function logoutUser() {
   localStorage.removeItem("accessToken");
   localStorage.removeItem("profile");
-  window.location.href = "/index.html";
+  window.location.href = "index.html";
 }
