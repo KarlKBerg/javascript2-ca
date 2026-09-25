0792cb721347094020f93b92333aa62d7473b636
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-25 17:32:48 +0200

    Moved pageCheck function and added it as an imported function from FeedController

--- src/script.js
+++ src/script.js
@@ -1,30 +1,10 @@
 "use strict";
+import { pageCheck } from "./components/FeedController.js";
 import { loginFormListener } from "./components/LoginForm.js";
 import { logoutUserListener } from "./components/LogoutForm.js";
 import { registerFormListener } from "./components/RegisterForm.js";
-import { renderFeed } from "./services/postsService.js";
 
 registerFormListener();
 loginFormListener();
 logoutUserListener();
-
-function pageCheck() {
-  const feed = document.getElementById("feed");
-  const token = localStorage.getItem("accessToken");
-  const loginPage = document.getElementById("login");
-
-  if (feed) {
-    if (!token) {
-      window.location.href = "index.html";
-    } else {
-      renderFeed();
-    }
-  }
-
-  if (loginPage) {
-    if (token) {
-      window.location.href = "feed.html";
-    }
-  }
-}
 pageCheck();
