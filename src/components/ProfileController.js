bbf565c4400a9914e8fe00f47950666439c6eb75
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-26 18:36:59 +0200

    Added function to add logged in username to url. Modified initProfile and initProfilePosts to read username from url, making every user able to see eachothers profile

--- src/components/ProfileController.js
+++ src/components/ProfileController.js
@@ -2,19 +2,39 @@ import { fetchProfile, fetchProfilePosts } from "../services/profileService.js";
 import { getProfile } from "../storage/storage.js";
 import { renderProfile } from "./Profile.js";
 import { renderPost } from "./PostCard.js";
+import { displayToastMessage } from "../utils/domHelpers.js";
 
 export async function initProfile() {
-  let username = getProfile().name;
-  let user = await fetchProfile(username);
-  renderProfile(user);
+  const url = new URLSearchParams(window.location.search);
+  const params = url.get("name");
+  let username = params;
+  if (!username) return;
+  try {
+    let user = await fetchProfile(username);
+    renderProfile(user);
+  } catch (error) {
+    displayToastMessage(error.message, "error");
+  }
 }
 
 export async function initProfilePosts() {
-  let username = getProfile().name;
-  let user = await fetchProfile(username);
-  let posts = await fetchProfilePosts(username);
-  posts.forEach((post) => {
-    renderPost(post, "user-posts-container");
-    console.log(post);
-  });
+  const url = new URLSearchParams(window.location.search);
+  const params = url.get("name");
+  let username = params;
+  try {
+    let posts = await fetchProfilePosts(username);
+    posts.forEach((post) => {
+      renderPost(post, "user-posts-container");
+    });
+  } catch (error) {
+    displayToastMessage(error.message, "error");
+  }
+}
+
+export function myProfile() {
+  const profileLink = document.getElementById("profile-link");
+  if (!profileLink) return;
+  const username = getProfile().name;
+  if (!username) return;
+  profileLink.href = `profile.html?name=${username}`;
 }
