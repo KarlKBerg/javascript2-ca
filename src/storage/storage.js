733f52c45c89199eaa77e2507744aec6db59b979
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-26 14:44:23 +0200

    Fixed setToken function to take parameter for token. Created getProfile function

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -6,6 +6,10 @@ export function removeToken() {
   return localStorage.removeItem("accessToken");
 }
 
-export function setToken() {
-  return localStorage.setItem("accessToken");
+export function setToken(token) {
+  return localStorage.setItem("accessToken", token);
+}
+
+export function getProfile() {
+  return JSON.parse(localStorage.getItem("profile"));
 }
