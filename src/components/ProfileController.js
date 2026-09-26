10bd0842176123afe5cfcbbb53fe935d694c506a
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-26 16:44:01 +0200

    Fixed wrong paramater in initProfilePosts, now it correctly takes post instead of user

--- src/components/ProfileController.js
+++ src/components/ProfileController.js
@@ -1,9 +1,20 @@
-import { fetchProfile } from "../services/profileService.js";
+import { fetchProfile, fetchProfilePosts } from "../services/profileService.js";
 import { getProfile } from "../storage/storage.js";
 import { renderProfile } from "./Profile.js";
+import { renderPost } from "./PostCard.js";
 
 export async function initProfile() {
   let username = getProfile().name;
   let user = await fetchProfile(username);
   renderProfile(user);
+}
+
+export async function initProfilePosts() {
+  let username = getProfile().name;
+  let user = await fetchProfile(username);
+  let posts = await fetchProfilePosts(username);
+  posts.forEach((post) => {
+    renderPost(post, "user-posts-container");
+    console.log(post);
+  });
 }
