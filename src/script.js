267cab4c604a1fc6195485e54c8a8fded665617d
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-26 11:11:32 +0200

    Imported and ran createPostListener

--- src/script.js
+++ src/script.js
@@ -3,8 +3,10 @@ import { pageCheck } from "./components/FeedController.js";
 import { loginFormListener } from "./components/LoginForm.js";
 import { logoutUserListener } from "./components/LogoutForm.js";
 import { registerFormListener } from "./components/RegisterForm.js";
+import { createPostListener } from "./components/CreatePostController.js";
 
 registerFormListener();
 loginFormListener();
 logoutUserListener();
 pageCheck();
+createPostListener();
