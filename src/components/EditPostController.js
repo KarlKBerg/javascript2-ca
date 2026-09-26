f11a6593779f3f5603587236d1311a60244c14f8
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-26 13:40:29 +0200

    Moved edit modal title and body fill to separate populateEditModal function that now runds alone in PostCard.js

--- src/components/EditPostController.js
+++ src/components/EditPostController.js
@@ -1,12 +1,14 @@
 import { updatePost } from "../services/postsService.js";
 import { displayToastMessage } from "../utils/domHelpers.js";
 import { renderFeed } from "./FeedController.js";
+import { editPostId } from "./PostCard.js";
 
 async function checkEditPost(id, updatedTitle, updatedBody) {
   const container = document.querySelector(".edit-post-modal");
   if (!updatedTitle) {
     container.classList.add("hidden");
     displayToastMessage("Post needs a title", "error");
+    return;
   }
   try {
     await updatePost(id, updatedTitle, updatedBody);
@@ -18,14 +20,11 @@ async function checkEditPost(id, updatedTitle, updatedBody) {
   }
 }
 
-export function editPostListener(id, postTitle, postBody) {
-  const titleField = document.getElementById("edit-post-title");
-  const bodyField = document.getElementById("edit-post-message");
+export function editPostListener() {
   const editForm = document.getElementById("edit-post");
   const cancelBtn = document.getElementById("cancel-edit-post-btn");
+  const container = document.querySelector(".edit-post-modal");
 
-  titleField.value = postTitle;
-  bodyField.value = postBody;
   cancelBtn.addEventListener("click", () => {
     container.classList.add("hidden");
   });
@@ -33,6 +32,13 @@ export function editPostListener(id, postTitle, postBody) {
     event.preventDefault();
     const newTitle = document.getElementById("edit-post-title").value.trim();
     const newBody = document.getElementById("edit-post-message").value.trim();
-    checkEditPost(id, newTitle, newBody);
+    checkEditPost(editPostId, newTitle, newBody);
   });
+}
+
+export function populateEditModal(postTitle, postBody) {
+  const titleField = document.getElementById("edit-post-title");
+  const bodyField = document.getElementById("edit-post-message");
+  titleField.value = postTitle;
+  bodyField.value = postBody;
 }
