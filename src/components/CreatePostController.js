8f0ba9923a994cd2809846f985733e7e9215aca7
Karl-Kristian Berg <karlkristianeberg@gmail.com>
2026-09-26 11:08:09 +0200

    Created controller for checking post before creation and eventlistener on form to submit the post

---
+++ src/components/CreatePostController.js
@@ -0,0 +1,26 @@
+import { createPost } from "../services/postsService.js";
+import { displayToastMessage } from "../utils/domHelpers.js";
+import { renderFeed } from "./FeedController.js";
+
+export async function checkCreatePost() {
+  const titleField = document.getElementById("post-title").value.trim();
+  const body = document.getElementById("post-message").value.trim();
+
+  if (titleField === "") return;
+
+  try {
+    await createPost(titleField, body);
+    renderFeed();
+  } catch (error) {
+    displayToastMessage(error.message, "error");
+  }
+}
+
+export function createPostListener() {
+  const form = document.getElementById("create-post-form");
+  if (!form) return;
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    checkCreatePost();
+  });
+}
